de4acaefa95cab356149e7147b5f5b94d6d14b6e
banjintaohua <fjxzyx@gmail.com>
2020-02-18 21:51:28 +0800

    修改 | 延迟一分钟启动 iPic

--- Bash/iPac.sh
+++ Bash/iPac.sh
@@ -2,5 +2,5 @@
 
 # shellcheck disable=SC2046
 /usr/libexec/PlistBuddy -c "Set ':values:Markdown Flag:value' $(date +%s) "  /Users/$(whoami)/Library/Containers/net.toolinbox.ipic/Data/Library/SyncedPreferences/net.toolinbox.ipic.plist
-sleep 10
+sleep 60
 nohup /Applications/iPic.app/Contents/MacOS/iPic >> /Users/sea/Documents/Config/Log/iPic.log 2>&1 &
