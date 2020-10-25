f4137d3c5c9884f887f49fc7917810c9d12f40f0
banjintaohua <fjxzyx@gmail.com>
2020-10-25 23:48:55 +0800

    refactor: use docker env param

--- GitHooks/post-receive.sh
+++ GitHooks/post-receive.sh
@@ -3,13 +3,13 @@
 command=$(
 cat <<-EOF
         echo 'step1: enter project path'
-        if [ ! -d '/usr/local/opt/hexo/source/_posts' ]; then
+        if [ ! -d '/usr/local/opt/hexo/blog/source/_posts' ]; then
           echo 'error: project uninitialized' && exit
         fi
-        cd /usr/local/opt/hexo/source/_posts && pwd
+        cd /usr/local/opt/hexo/blog/source/_posts && pwd
 
         echo 'step2: get project'
-        if [ ! -d '/usr/local/opt/hexo/source/_posts/.git' ]; then
+        if [ ! -d '/usr/local/opt/hexo/blog/source/_posts/.git' ]; then
           cd ../
           echo 'clone project'
           git clone https://$REPOSITORY_USER:$REPOSITORY_PASSWORD@$PROJECT_ADDRESS
@@ -20,7 +20,7 @@ cat <<-EOF
         fi
 
         echo 'step3: update project'
-        git fetch origin && git reset --hard origin/master && git clean -fd
+        git fetch origin && git reset --hard origin/\$BRANCH && git clean -fd
 
         echo 'check hexo server status'
         ps | grep hexo
@@ -30,6 +30,7 @@ cat <<-EOF
         echo 'step4: generate static files and start hexo server'
         hexo clean
         hexo generate
+        hexo deploy
 
         if [[ \$process -lt 1 ]]; then
             echo 'starting hexo server'
